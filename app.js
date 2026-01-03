3ebab76156c078c1ccbc1d3f9ee9662b33f1d6fd
Safiy <safiy@example.com>
2026-01-03 14:08:57 +0530

    Initial Commit: Basic Express app setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req,res) => {
+    res.send("welcome to shop easy");
+})
+
+app.listen(PORT, () =>{
+    console.log(`your server is running on port ${PORT}`);
+})
